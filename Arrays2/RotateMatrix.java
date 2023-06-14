c47a92740a840e13c03c1c19252121861a0cbdda
Barnwal-Sourabh <sourabhbarnwal2001@gmail.com>
2023-06-14 22:53:23 +0530

    rotate matrix

---
+++ Arrays2/RotateMatrix.java
@@ -0,0 +1,47 @@
+package Arrays2;
+
+/**
+ * RotateMatrix
+ */
+public class RotateMatrix {
+
+    public static void main(String[] args) {
+        int arr[][] =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotate(arr);
+        System.out.println("Rotated Image");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void rotate(int[][] matrix) {
+        transpose(matrix);
+        reflect(matrix);
+    }
+
+    public static void transpose(int[][] matrix){
+        int n = matrix.length;
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+    public static void reflect(int[][] matrix){
+        int n = matrix.length;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
+        }
+    }
+}
