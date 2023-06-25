55b6f22c7e7d839013c7a463585ef1e477d5c496
Barnwal-Sourabh <sourabhbarnwal2001@gmail.com>
2023-06-25 23:23:20 +0530

    optimize solution

--- Array4/LargestSubarrayWithKSum.java
+++ Array4/LargestSubarrayWithKSum.java
@@ -1,23 +1,33 @@
-
+import java.util.*;
 
 public class LargestSubarrayWithKSum {
-static int solve(int[] a){
-	int  max = 0;
-	for(int i = 0; i < a.length; ++i){
+
+	private static int maxLen(int arr[], int n) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+
+		int maxLen = 0;
 		int sum = 0;
-		for(int j = i; j < a.length; ++j){
-			sum += a[j];
-			if(sum == 0){
-				max = Math.max(max, j-i+1);
+
+		for (int i = 0; i < n; i++) {
+			sum += arr[i];
+
+			if (sum == 0) {
+				maxLen = i + 1;
+			} else {
+				if (map.containsKey(sum)) {
+					maxLen = Math.max(maxLen, i - map.get(sum));
+				} else {
+					map.put(sum, i);
+				}
+
 			}
 		}
-	}
-	return max;
-   }
 
-    public static void main(String args[]) 
-    { 
-        int a[] = {9, -3, 3, -1, 6, -5};
-        System.out.println(solve(a));
-    } 
+		return maxLen;
+	}
+
+	public static void main(String args[]) {
+		int a[] = { 9, -3, 3, -1, 6, -5 };
+		System.out.println(maxLen(a, a.length));
+	}
 }
