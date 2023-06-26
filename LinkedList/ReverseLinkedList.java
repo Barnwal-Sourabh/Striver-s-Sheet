149a02983c15f9ca1d50d5320e51ec5cd8fd7a93
Barnwal-Sourabh <sourabhbarnwal2001@gmail.com>
2023-06-26 23:02:19 +0530

    reverse linked list

---
+++ LinkedList/ReverseLinkedList.java
@@ -0,0 +1,47 @@
+package LinkedList;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        //Approch 1
+        // ListNode curr = head;
+        // ListNode prev = null;
+        // ListNode fwd = null;
+
+        // while(curr != null){
+        // fwd = curr.next;
+        // curr.next = prev;
+        // prev = curr;
+        // curr = fwd;
+        // }
+        // return prev;
+
+        //Approch 2
+        ListNode    temp = head.next;
+        ListNode ans = reverseList(head.next);
+        temp.next = head;
+        head.next = null;
+        return ans;
+    }
+}
