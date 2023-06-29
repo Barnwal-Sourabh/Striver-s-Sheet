0bb26cac7ede924bdf7feee51fad5ace3558912f
Barnwal-Sourabh <sourabhbarnwal2001@gmail.com>
2023-06-29 23:58:34 +0530

    rotate linked list

---
+++ LinkedListAndArrays/RotateaLinkedList.java
@@ -0,0 +1,46 @@
+package LinkedListAndArrays;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class RotateaLinkedList {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || k == 0 || head.next == null) {
+            return head;
+        }
+
+        int length = 1;
+        ListNode temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+            ++length;
+        }
+
+        temp.next = head;
+        k = k % length; // when k is more than length of list
+        int end = length - k; // to get end of the list
+        while (end-- != 0)
+            temp = temp.next;
+        // breaking last node link and pointing to NULL
+        head = temp.next;
+        temp.next = null;
+
+        return head;
+
+    }
+}
